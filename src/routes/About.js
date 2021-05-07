427ad89de8a6c740464e208bb65552db0d27ba9a
이름 <>
2021-05-08 00:30:31 +0900

    create movie app last commit

---
+++ src/routes/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.css';
+
+function About(){
+    return <div className="about_container">
+        <span>헿헿헿헿헿헿헿헿헿헿</span>
+        <span>헿헿헿헿헿헿헿헿헿헿</span>
+        <span>헿헿헿헿헿헿헿헿헿헿</span>
+    </div>
+}
+
+export default About;
